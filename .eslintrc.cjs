e53074a060a67d5df8d5d72d57d33dba76fefdd5
Sam Law <samuel.law@system1group.com>
2024-03-17 11:54:44 +0000

    Updated ESLint rules to allow `any`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,7 +36,9 @@ const config = {
           "attributes": false
         }
       }
-    ]
+    ],
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   }
 }
 module.exports = config;
