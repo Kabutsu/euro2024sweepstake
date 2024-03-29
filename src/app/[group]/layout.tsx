5729f45ffad5d664d4a378b6244986b3ea360fb1
Sam Law <samuel.law@system1group.com>
2024-03-29 20:40:53 +0000

    Mobile styles

--- src/app/[group]/layout.tsx
+++ src/app/[group]/layout.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Layout({ children, params: { group: groupId } }: { children: React.ReactNode, params: { group: string } }) {
@@ -5,13 +6,22 @@ export default function Layout({ children, params: { group: groupId } }: { child
   
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-row justify-between w-full h-16 p-4 pr-8 shadow-md">
-        <h1 className="font-semibold text-base">
-          Group Name
-        </h1>
+      <div className="flex flex-row justify-between w-full h-auto sm:h-16 p-4 pr-8 shadow-md">
+        <div className="flex flex-row items-center gap-4">
+          <Image src="/images/bars-solid.svg" alt="menu" width={0} height={0} className="h-6 w-6 cursor-pointer sm:hidden" />
+          <h1 className="font-semibold text-xl sm:text-base truncate">
+            Group Name
+          </h1>
+        </div>
         <nav className="flex flex-row gap-6 font-light text-base">
-          <Link href="draw" className="min-w-16 text-center">Draw</Link>
-          <Link href="chat" className="min-w-16 text-center">Chat</Link>
+          <Link href="chat" className="sm:min-w-16 text-center">
+            <Image src="/images/comments-solid.svg" alt="draw" width={0} height={0} className="sm:hidden h-7 w-7 px-1" />
+            <p className="hidden sm:inline">Chat</p>
+          </Link>
+          <Link href="draw" className="sm:min-w-16 text-center">
+            <Image src="/images/circle-info-solid.svg" alt="draw" width={0} height={0} className="sm:hidden h-7 w-7 px-1" />
+            <p className="hidden sm:inline">Draw</p>
+          </Link>
         </nav>
       </div>
       <div className="flex flex-1">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from "next/link";
 
 import { getServerAuthSession } from "~/server/auth";
@@ -5,13 +6,29 @@ import { getServerAuthSession } from "~/server/auth";
 export default async function Home() {
   const session = await getServerAuthSession();
 
+  const selectAGroup = (
+    <>
+      <span className="sm:hiden inline">Hit <Image src="/images/bars-solid.svg" alt="menu" width={0} height={0} className="w-6 h-6 pb-1 inline"/> to select a group</span>
+      <span className="hidden sm:inline">Select a group</span>
+    </>
+  );
+
+  const signInButton = (
+    <Link
+      href="/api/auth/signin"
+      className="rounded-full bg-black/10 px-3 py-1 sm:px-4 sm:py-3 mr-1 sm:mr-2 text-xl sm:text-2xl font-semibold no-underline transition hover:bg-black/20"
+    >
+      Sign in
+    </Link>
+  );
+
   return (
     <div className="relative flex min-h-screen flex-col items-center bg-gradient-to-b from-[rgb(249,250,252)] to-[rgb(242,243,246)] text-gray-950">
       <div className="container flex flex-col items-center justify-center gap-12 md:gap-24 px-4 pb-16 pt-20 ">
         <h1 className="text-5xl font-extrabold tracking-tight md:text-[5rem] text-center">
           Euro <span className="text-[#1963E0]">2024</span> Sweepstake
         </h1>
-        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:gap-32">
+        <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:gap-32 text-center sm:text-left">
           <div className="flex max-w-64 flex-col gap-4 p4">
             <h3 className="text-3xl font-bold">Get Involved</h3>
             <div className="text-xl">
@@ -25,28 +42,29 @@ export default async function Home() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl">
-            {session
-              ? 'Select a group '
-              : (
-                <Link
-                  href="/api/auth/signin"
-                  className="rounded-full bg-black/10 px-6 py-3 mr-2 font-semibold no-underline transition hover:bg-black/20"
-                >
-                  Sign in
-                </Link>
-            )} to get started.
+        <div className="flex flex-col items-center gap-2 text-center sm:text-left">
+          <p className="text-2xl px-10 sm:px-0">
+            {session ? selectAGroup : signInButton} and get started.
           </p>
         </div>
       </div>
       {session && (
-        <Link
-          href="/api/auth/signout"
-          className="absolute top-6 right-8 rounded-full bg-black/10 px-4 py-2 font-semibold no-underline transition hover:bg-black/20"
-        >
-          Sign out
-        </Link>
+        <>
+          <Image
+            src="/images/bars-solid.svg"
+            alt="menu"
+            width={0}
+            height={0}
+            className="absolute top-6 left-8 h-9 w-9 cursor-pointer sm:hidden"
+          />
+
+          <Link
+            href="/api/auth/signout"
+            className="absolute top-6 right-8 rounded-full bg-black/10 px-4 py-2 font-semibold no-underline transition hover:bg-black/20"
+          >
+            Sign out
+          </Link>
+        </>
       )}
     </div>
   );
