5dceedf268882bcbccaf8585d8359a81f9cfe5cd
Sam Law <samuel.law@system1group.com>
2024-04-27 17:17:05 +0100

    Card redesign WIP

--- src/app/[group]/info/_components/user-card.tsx
+++ src/app/[group]/info/_components/user-card.tsx
@@ -5,8 +5,6 @@ import Image from 'next/image';
 
 import { type Users } from '~/server/api/root';
 
-import { CircleChevronDownIcon } from '~/app/_components/icons';
-
 type User = Users[number];
 
 type Props = {
@@ -38,7 +36,7 @@ const UserCard = ({ user }: Props) => {
     <div className="flex flex-col flex-1">
       <div ref={containerRef} className="flex flex-row rounded-lg shadow-md bg-gray-50 cursor-pointer w-full sm:w-[60%] sm:hover:scale-105 transition-transform duration-300">
         <Image
-          src={user.image ?? "/images/icons/user-solid.svg"}
+          src={user.image ?? "/images/user-solid.svg"}
           alt={user.name ?? user.id}
           width={250}
           height={250}
@@ -47,9 +45,7 @@ const UserCard = ({ user }: Props) => {
         <div className="flex flex-col flex-1 p-4 pl-0 sm:pl-4 truncate">
           <h2 className="text-xl font-semibold truncate">{user.name}</h2>
           <p className="hidden sm:block text-lg font-normal truncate">{user.email}</p>
-          <button onClick={toggleExpanded} className="sm:hidden w-6 h-6 mt-1">
-            <CircleChevronDownIcon className="sm:hidden w-full h-full fill-[#1963E0]" />
-          </button>
+          <Image src="/images/flags/spain-flag-square-icon-128.png" alt="Flag" className="sm:hidden rounded-full w-6 h-6 mt-1" />
         </div>
       </div>
       {isExpanded && (
